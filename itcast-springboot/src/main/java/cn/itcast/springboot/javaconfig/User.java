ece2e8953df0dc1a59c768504f4b4d60fe0dbf57
jacksonary <jacksonary@163.com>
2017-11-27 09:29:36 +0800

    1.0

---
+++ itcast-springboot/src/main/java/cn/itcast/springboot/javaconfig/User.java
@@ -0,0 +1,35 @@
+package cn.itcast.springboot.javaconfig;
+
+public class User {
+
+    private String username;
+
+    private String password;
+
+    private Integer age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+}
