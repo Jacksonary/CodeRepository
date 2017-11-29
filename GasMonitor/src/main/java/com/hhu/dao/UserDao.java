db5047db393f09a79af467ac0693c31e20faaef0
jacksonary <jacksonary@163.com>
2017-11-29 22:22:44 +0800

    删除监控系统

--- GasMonitor/src/main/java/com/hhu/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.hhu.dao;
-
-import com.hhu.entity.User;
-
-public interface UserDao {
-
-	public User getUser(String userName, String password);
-	
-}
--- GasMonitor/src/main/java/com/hhu/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.hhu.service;
-
-import com.hhu.entity.User;
-
-public interface UserService {
-
-	public User getUser(String userName, String password);
-	
-}
